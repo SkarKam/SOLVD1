c63679d46c1157d8751d2bae02e7f5d05fb42189
GitGutPJ <s25020@pjwstk.edu.pl>
2024-08-06 17:07:14 +0200

    Finished v. 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Arrays;
+/*
+To run in terminal:
+javac Main.java
+java Main.java 9 3 0 2 1 45 44
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] tab = new int[args.length];
+        for (int i = 0; i < args.length; i++) {
+            tab[i] = Integer.parseInt(args[i]);
+        }
+        System.out.println("Table before sorting algorithm: " + Arrays.toString(tab));
+
+        System.out.println("Table after sorting algorithm: " + Arrays.toString(sorting(tab)));
     }
+
+    /**
+     * Sorting method
+     * @param tab - table to be sorted
+     * @return sorted table
+     */
+    public static int[] sorting(int[] tab){
+        for(int i = 0; i<tab.length; i++)
+            for(int j = 0; j<tab.length; j++)
+            {
+                if(tab[j] > tab[i]){
+                    int temp = tab[j];
+                    tab[j] = tab[i];
+                    tab[i] = temp;
+                }
+            }
+       return tab;
+    }
+
 }
